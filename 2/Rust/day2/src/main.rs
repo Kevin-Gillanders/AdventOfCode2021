ca8dd4c4a471bc7d11732969905b7e8de18205d8
Kevin Gillanders <kevin.gillanders@blueinsurance.ie>
2022-06-09 15:34:42 +0100

    fmt

--- 2/Rust/day2/src/main.rs
+++ 2/Rust/day2/src/main.rs
@@ -1,97 +1,87 @@
-use std::io::{BufReader, BufRead};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 
 #[derive(Debug)]
-enum Movement{
+enum Movement {
     Up,
     Down,
     Forward,
 }
 
-
-impl FromStr for Movement{
-    
+impl FromStr for Movement {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let lowercase = s.to_lowercase();
         match lowercase.as_str() {
-            "up"      => Ok(Movement::Up), 
-            "down"    => Ok(Movement::Down), 
+            "up" => Ok(Movement::Up),
+            "down" => Ok(Movement::Down),
             "forward" => Ok(Movement::Forward),
-            _         => Err(()), 
+            _ => Err(()),
         }
     }
 }
-
-
 
 fn part1(buf_reader: BufReader<File>) -> u32 {
     let mut horizontal = 0;
     let mut depth = 0;
 
-    for line in buf_reader.lines(){
-        let tmp_line = match line{
+    for line in buf_reader.lines() {
+        let tmp_line = match line {
             Ok(l) => l,
             Err(_error) => panic!("problem with line"),
         };
-        let line_result : Vec<&str>= tmp_line.split(" ").collect();
+        let line_result: Vec<&str> = tmp_line.split(" ").collect();
         // println!("{:?}", line_result);
         let line_enum: Movement = line_result[0].parse().unwrap();
         // println!("as enum {:?}", line_enum );
-        let movement: u32 = line_result[1].parse().expect("Not a number"); 
+        let movement: u32 = line_result[1].parse().expect("Not a number");
 
         match line_enum {
-            Movement::Up        => depth = depth - movement, 
-            Movement::Down      => depth = depth + movement, 
-            Movement::Forward   => horizontal = horizontal + movement
+            Movement::Up => depth = depth - movement,
+            Movement::Down => depth = depth + movement,
+            Movement::Forward => horizontal = horizontal + movement,
         };
         // println!("depth {:?}, horizontal {:?}", depth, horizontal );
     }
-    
+
     // println!("Multiplied Result : {:?}", depth * horizontal);
     return depth * horizontal;
 }
-
 
 fn part2(buf_reader: BufReader<File>) -> u32 {
     let mut horizontal = 0;
     let mut depth = 0;
     let mut aim = 0;
 
-    for line in buf_reader.lines(){
-        let tmp_line = match line{
+    for line in buf_reader.lines() {
+        let tmp_line = match line {
             Ok(l) => l,
             Err(_error) => panic!("problem with line"),
         };
-        let line_result : Vec<&str>= tmp_line.split(" ").collect();
+        let line_result: Vec<&str> = tmp_line.split(" ").collect();
         // println!("{:?}", line_result);
         let line_enum: Movement = line_result[0].parse().unwrap();
         // println!("as enum {:?}", line_enum );
-        let movement: u32 = line_result[1].parse().expect("Not a number"); 
+        let movement: u32 = line_result[1].parse().expect("Not a number");
 
         match line_enum {
-            Movement::Up        => aim = aim - movement, 
-            Movement::Down      => aim = aim + movement, 
-            Movement::Forward   => {
-                                        horizontal = horizontal + movement;
-                                        depth = depth + (aim * movement);
-                                   }
+            Movement::Up => aim = aim - movement,
+            Movement::Down => aim = aim + movement,
+            Movement::Forward => {
+                horizontal = horizontal + movement;
+                depth = depth + (aim * movement);
+            }
         };
         // println!("depth {:?}, horizontal {:?} aim{:?}", depth, horizontal, aim );
     }
-    
+
     // println!("Multiplied Result : {:?}", depth * horizontal);
     return depth * horizontal;
 }
 
-
-
 fn main() -> std::io::Result<()> {
-
-
-
     let file = File::open("input.txt")?;
     let buf_reader = BufReader::new(file);
     println!("Part1 answer {:?}", part1(buf_reader));
@@ -100,8 +90,5 @@ fn main() -> std::io::Result<()> {
     let buf_reader = BufReader::new(file);
     println!("Part2 answer {:?}", part2(buf_reader));
 
-
     Ok(())
-
-
 }
